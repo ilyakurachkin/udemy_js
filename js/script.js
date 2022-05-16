ce74af35366a2be8cc6c58a4adc43d669e044800
Ilya Kurachkin <patlatov@inbox.ru>
2022-05-16 21:12:37 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
 "use strict";
 
-const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
-const a = prompt('Один из последних посмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних посмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
+// const a = prompt('Один из последних посмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних посмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// const zhenya = 90;
+
+// if (zhenya < 55) {
+//     console.log('Молодец Женя');
+// } else if (zhenya > 57) {
+//     console.log('много ешь Женя');
+// } else if (zhenya == 56) {
+//     console.log('Молодец Женя');
+// }
 
 
 
+const hamburger = 3;
+const fries = '';
+
+if (hamburger === 3 && fries) {
+    console.log('Мы сыты');
+} else {
+    console.log('мы уходим');
+}
