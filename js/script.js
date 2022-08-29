5670bf15a29ea0e626fe5d39cc8dab32d5731087
ilyakurachkin <patlatov@inbox.ru>
2022-08-29 20:45:06 +0300

    Tasks and For in

--- js/script.js
+++ js/script.js
@@ -40,8 +40,26 @@
 //     console.log('мы уходим');
 // }
 
-let i = 0
-
-for (i=0;i<=3;i++) {
-    console.log(i)
+for (let i = 0; i <= 3; i++) {
+    console.log(i);
+    for (let j = 0; j <= 3; j++)
+    console.log(j);
 }
+
+//*
+//** 
+//*** 
+//**** 
+//***** 
+//******
+
+let result = '';
+const length = 7;
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j<i;j++)
+    result += '*';
+
+    result += '\n';
+}
+
+console.log(result);
