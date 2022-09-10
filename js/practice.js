bbef3b808524f6e9585111d051fa8244d4a8eadf
ilyakurachkin <patlatov@inbox.ru>
2022-09-10 14:30:19 +0300

    test

--- js/practice.js
+++ js/practice.js
@@ -20,3 +20,88 @@ for (let i = 0; i < 2; i++) {
 console.log(personalMovieDB);
 
 
+// 'use strict'
+
+// let num = 20;
+
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 10
+//     console.log(num)
+// }
+
+// showFirstMessage('hello world');
+// console.log(num)
+
+
+// function calc(a,b) {
+//     return (
+//         a + b 
+//     )
+// }
+
+// console.log(calc(1,5))
+
+
+
+
+
+
+// function ret() {
+//   num = 50
+  
+  
+//   return num;
+// }
+
+// const oneNumber = ret();
+// console.log(oneNumber)
+
+
+
+
+
+
+// const logger = function art() {
+//     console.log('HELLO')
+// }; 
+// logger()
+
+
+
+// const calcc = (a,b) => {
+//      a + b ;
+// }
+// const summ = calcc(3,2)
+
+// console.log(summ)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
