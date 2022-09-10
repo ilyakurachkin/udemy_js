d7cecf2b431687b6f07f3649e102d001a528afb5
ilyakurachkin <patlatov@inbox.ru>
2022-09-10 16:11:01 +0300

    Practic For,while, only forward to meet the stars

--- js/practice.js
+++ js/practice.js
@@ -10,72 +10,44 @@ const personalMovieDB = {
 };
 
 
-for (let i = 0; i < 2; i++) {
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних посмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+//         personalMovieDB.movies[a] = b;
+//     if ( a != null && b !=null && a != '' && b != '' && a.length < 50) {
+//         console.log('Done') 
+//       }  else {
+//         console.log('error')
+//         i--
+//     }
+//---Для закрепления 
+let i = 0;
+while(i < 2) {
+    i++
     const a = prompt('Один из последних посмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[a] = b;
-а
+          b = prompt('На сколько оцените его?', '');
+          personalMovieDB.movies[a] = b;
+
+        if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+            console.log('Done')
+        } else {
+            console.log('Error')
+            i--
+        }
 }
+
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрино довольно мало фильмов') 
+         } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+            console.log('Вы классический зритель')
+        } else if (personalMovieDB.count > 30) {
+            console.log('Вы киноман') 
+        } else {
+            console.log('Произошла ошибка')
+        }
 
 console.log(personalMovieDB);
 
-
-// 'use strict'
-
-// let num = 20;
-
-
-// function showFirstMessage(text) {
-//     console.log(text);
-//     let num = 10
-//     console.log(num)
-// }
-
-// showFirstMessage('hello world');
-// console.log(num)
-
-
-// function calc(a,b) {
-//     return (
-//         a + b 
-//     )
-// }
-
-// console.log(calc(1,5))
-
-
-
-
-
-
-// function ret() {
-//   num = 50
-  
-  
-//   return num;
-// }
-
-// const oneNumber = ret();
-// console.log(oneNumber)
-
-
-
-
-
-
-// const logger = function art() {
-//     console.log('HELLO')
-// }; 
-// logger()
-
-
-
-// const calcc = (a,b) => {
-//      a + b ;
-// }
-// const summ = calcc(3,2)
-
-// console.log(summ)
 
 
 
